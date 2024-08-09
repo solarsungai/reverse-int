656a29654a0e5bf0bb107cced10b592c483f5c09
solarsungai <solarsungai@gmail.com>
2024-08-09 12:27:25 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+
+    const absNumber = Math.abs(n);
+    const firstDigit = String(absNumber)[0];
+    const secondDigit = String(absNumber)[1];
+    const lastDigit = absNumber % 10;
+
+    const string = String(lastDigit) + secondDigit + firstDigit;
+
+    return Number(string);
+};
